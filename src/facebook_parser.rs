98d009bac18d891967216f4cd91b32e2aa70a1a1
Sean McBride <spmcbride1201@gmail.com>
2019-11-02 15:18:48 -0400

    fix: don't crash when FB data is missing

--- src/facebook_parser.rs
+++ src/facebook_parser.rs
@@ -10,7 +10,9 @@ pub struct FBFriends {
 
 #[derive(Deserialize)]
 pub struct FBFriend {
+    #[serde(default)]
     pub name: String,
+    #[serde(default)]
     pub target: String,
 }
 
@@ -35,7 +37,6 @@ pub struct Profile {
     pub birthday: Date,
     pub gender: Gender,
     pub previous_names: Vec<TimestampedString>,
-    // Unclear on how other_names works
     pub current_city: TimestampedString,
     pub hometown: TimestampedString,
     pub relationship: Relationship,
@@ -45,16 +46,18 @@ pub struct Profile {
     pub languages: Vec<String>,
     pub political_view: View,
     pub religious_view: View,
+    #[serde(default)]
     pub professional_skills: Vec<String>,
     pub address: Address,
     pub phone_numbers: Vec<PhoneNumber>,
+    #[serde(default)]
     pub username: String,
     pub places_lived: Vec<PlaceLived>,
-    // Ignore Favorite Quotes
+    #[serde(default)]
     pub name_pronunciation: String,
-    // Ignore Pages
-    // Ignore Groups
+    #[serde(default)]
     pub profile_uri: String, // Is there a URL type?
+    #[serde(default)]
     pub intro_bio: String,
 }
 
@@ -75,61 +78,65 @@ impl FBProfileInformation {
 
 #[derive(Deserialize, Debug)]
 pub struct Name {
+    #[serde(default)]
     pub full_name: String,
+    #[serde(default)]
     pub first_name: String,
+    #[serde(default)]
     pub middle_name: String,
+    #[serde(default)]
     pub last_name: String,
 }
 #[derive(Deserialize, Debug)]
 pub struct Emails {
+    #[serde(default)]
     pub emails: Vec<String>,
+    #[serde(default)]
     pub previous_emails: Vec<String>,
+    #[serde(default)]
     pub pending_emails: Vec<String>,
+    #[serde(default)]
     pub ad_account_emails: Vec<String>,
 }
 #[derive(Deserialize, Debug)]
 pub struct Date {
+    #[serde(default)]
     pub year: u16,
+    #[serde(default)]
     pub month: u8,
+    #[serde(default)]
     pub day: u8,
 }
 #[derive(Deserialize, Debug)]
 pub struct Gender {
+    #[serde(default)]
     pub gender_option: String,
+    #[serde(default)]
     pub pronoun: String,
 }
 #[derive(Deserialize, Debug)]
 pub struct TimestampedString {
+    #[serde(default)]
     pub name: String,
+    #[serde(default)]
     pub timestamp: u64,
 }
 #[derive(Deserialize, Debug)]
 pub struct Relationship {
+    #[serde(default)]
     pub status: String,
+    #[serde(default)]
     pub partner: String,
     pub anniversary: Date,
 }
 #[derive(Deserialize, Debug)]
 pub struct FamilyMember {
+    #[serde(default)]
     pub name: String,
+    #[serde(default)]
     pub relation: String,
 }
 
-// #[derive(Deserialize)]
-// pub struct EducationExperience {
-//     pub name: String,
-//     #[serde(default)]
-//     pub start_timestamp: u64,
-//     #[serde(default)]
-//     pub end_timestamp: u64,
-//     pub graduated: bool,
-//     #[serde(default)]
-//     pub description: String,
-//     pub concentrations: Vec<String>,
-//     #[serde(default)]
-//     pub degree: String,
-//     pub school_type: String,
-// }
 #[derive(Deserialize, Debug)]
 #[serde(tag = "school_type")]
 pub enum EducationExperience {
@@ -181,39 +188,58 @@ pub enum EducationExperience {
 // TODO: Determine how "I currently work here" is encoded
 #[derive(Deserialize, Debug)]
 pub struct WorkExperience {
+    #[serde(default)]
     pub employer: String,
+    #[serde(default)]
     pub title: String,
     #[serde(default)]
     pub location: String,
+    #[serde(default)]
     pub description: String,
+    #[serde(default)]
     pub start_timestamp: u64,
+    #[serde(default)]
     pub end_timestamp: u64,
 }
 
 // Used for political and religious views
 #[derive(Deserialize, Debug)]
 pub struct View {
+    #[serde(default)]
     pub name: String,
+    #[serde(default)]
     pub description: String,
 }
 #[derive(Deserialize, Debug)]
 pub struct Address {
+    #[serde(default)]
     pub street: String,
+    #[serde(default)]
     pub city: String,
+    #[serde(default)]
     pub zipcode: String,
+    #[serde(default)]
     pub neighborhood: String,
+    #[serde(default)]
     pub country: String,
+    #[serde(default)]
     pub country_code: String,
+    #[serde(default)]
     pub region: String,
 }
 #[derive(Deserialize, Debug)]
 pub struct PhoneNumber {
+    #[serde(default)]
     pub phone_type: String,
+    #[serde(default)]
     pub phone_number: String,
+    #[serde(default)]
     pub verified: bool,
 }
 #[derive(Deserialize, Debug)]
 pub struct PlaceLived {
+    #[serde(default)]
     pub place: String,
+    #[serde(default)]
     pub start_timestamp: u64, // No clear end_timestamp
 }
